50baa313fb5b6738064a9013b423165207b86332
scurry <scurry@datalliance.com>
2013-12-19 15:28:26 -0500

    Initial commit

---
+++ src/main/java/io/krypto/obgen/App.java
@@ -0,0 +1,13 @@
+package io.krypto.obgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
