d6aaf9c9843af8025a5445b0d732cddbee4bf956
Curryst <scurry@datalliance.com>
2013-12-19 16:15:44 -0500

    Eclipse setup.

---
+++ src/main/java/io/krypto/obgen/ObGen.java
@@ -0,0 +1,16 @@
+package io.krypto.obgen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ObGen extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		// TODO
+	}
+
+	@Override
+	public void onDisable(){
+		// TODO
+	}
+}
